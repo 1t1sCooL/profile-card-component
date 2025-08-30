57e9f84ee6139378f9855492735db938104523f8
1t1sCooL <mmalabugin@gmail.com>
2025-08-30 16:15:49 +0300

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+import {Footer} from "./Footer.tsx";
+
+function App() {
+
+  return (<>
+      Victor Crest
+  26
+  London
+
+  80K
+  Followers
+
+  803K
+  Likes
+
+  1.4K
+  Photos
+
+<Footer/>
+      </>
+  )
+}
+
+export default App
