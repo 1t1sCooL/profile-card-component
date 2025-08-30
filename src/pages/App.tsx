e387d33cab864223712b1bb0496988de6f22cb6e
1t1sCooL <malabugin@anykey.pro>
2025-08-30 19:57:39 +0300

    update styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,25 +1,44 @@
-import './App.css'
+import styles from './App.module.css'
 import {Footer} from "./Footer.tsx";
 
 function App() {
 
   return (<>
-      Victor Crest
-  26
-  London
+  <div className={styles.container}>
+    <div className={styles.wrapper}>
+      <h1 className={styles.hidden}>Victor Crest</h1>
+      <img src={'/images/bg-pattern-card.svg'} alt="" />
+      <div className={styles.content}>
+        <img src={'/images/image-victor.jpg'}
+             alt='Victor Crest'/>
+        <h2>Victor Crest <span>26</span></h2>
+        <p>London</p>
+        <div className={styles.cardFooter}>
+          <div>
+            <p className={styles.num}>80K</p>
+            <p>Followers</p>
+          </div>
+          <div>
+            <p className={styles.num}>803K</p>
+            <p>Likes</p>
+          </div>
+          <div>
+            <p className={styles.num}>1.4K</p>
+            <p>Photos</p>
+          </div>
+        </div>
+      </div>
+      </div>
 
-  80K
-  Followers
+    </div>
 
-  803K
-  Likes
 
-  1.4K
-  Photos
 
-<Footer/>
-      </>
-  )
+
+
+  <Footer/>
+</>
+)
 }
 
 export default App
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,8 +1,12 @@
+import styles from './App.module.css'
+
 export const Footer = () => {
     return(
-        <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="#">Your Name Here</a>.
-        </div>
+        <footer>
+            <div className={styles.footer}>
+                <div>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.</div>
+                    <div>Coded by <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>.</div>
+            </div>
+        </footer>
     )
 }
